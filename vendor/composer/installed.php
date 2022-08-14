378b9d368facfce742a7056f1c61ddbcc4ead625
Lazuardi Fatahilah <lazuardifath@gmail.com>
2022-08-14 14:50:47 +0700

    change

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'ff9737da0bc41cc77d683034be5f3b9ff7ebfa58',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'ff9737da0bc41cc77d683034be5f3b9ff7ebfa58',
             'dev_requirement' => false,
         ),
         'midtrans/midtrans-php' => array(
